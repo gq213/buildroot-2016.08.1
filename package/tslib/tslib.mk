abd40766a66dddcff636e359c59bac2def4eaaff
gq213 <gaoqiang1211@gmail.com>
2018-06-17 23:18:55 +0800

    add tslib.

--- package/tslib/tslib.mk
+++ package/tslib/tslib.mk
@@ -13,4 +13,12 @@ TSLIB_AUTORECONF = YES
 TSLIB_INSTALL_STAGING = YES
 TSLIB_INSTALL_STAGING_OPTS = DESTDIR=$(STAGING_DIR) LDFLAGS=-L$(STAGING_DIR)/usr/lib install
 
+ifeq ($(BR2_PACKAGE_TSLIB),y)
+define SETTING_TSLIB_ENV
+	$(INSTALL) -D -m 0755 package/tslib/S50tslib.sh \
+		$(TARGET_DIR)/etc/init.d
+endef
+TARGET_FINALIZE_HOOKS += SETTING_TSLIB_ENV
+endif
+
 $(eval $(autotools-package))
